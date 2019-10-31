498a8048ae2ed873caecbd631373c1558e384b8d
Drew Hartberg <drewhartberg@gmail.com>
2019-10-31 18:31:27 +0000

    Sections

--- app/controllers/instructor/sections_controller.rb
+++ app/controllers/instructor/sections_controller.rb
@@ -1,6 +1,6 @@
 class Instructor::SectionsController < ApplicationController
 before_action :authenticate_user!
-before_action :require_authorized_for_current_course, only: [:create]
+before_action :require_authorized_for_current_course, only: [:new, :create]
 before_action :require_authorized_for_current_section, only: [:update]
 
   def new
@@ -37,7 +37,11 @@ before_action :require_authorized_for_current_section, only: [:update]
 
   helper_method :current_course
   def current_course
-    @current_course ||= Course.find(params[:course_id])
+    if params[:course_id]
+      @current_course ||= Course.find(params[:course_id])
+    else 
+      current_section.course
+    end
   end
 
   def section_params
